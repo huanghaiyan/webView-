e599cacbfb03ef7154b44e232b9d3878b6664724
huanghaiyan <qq1531398577@126.com>
2016-05-25 12:25:01 +0800

    修改警告

--- webView使用/ViewController.m
+++ webView使用/ViewController.m
@@ -10,7 +10,7 @@
 
 @interface ViewController ()<UIWebViewDelegate>
 {
-    UIWebView* webView;
+    UIWebView *mWebView;
 }
 @end
 
@@ -21,14 +21,14 @@
     // Do any additional setup after loading the view, typically from a nib.
     //创建UIWebView
     CGRect bounds = CGRectMake(0,0, self.view.bounds.size.width, self.view.bounds.size.height - 100);
-    webView = [[UIWebView alloc]initWithFrame:bounds];
+    mWebView = [[UIWebView alloc]initWithFrame:bounds];
     //设置属性
-    webView.scalesPageToFit = YES;//自动对页面进行缩放以适应屏幕
-    [webView setUserInteractionEnabled:YES];//是否支持交互
+    mWebView.scalesPageToFit = YES;//自动对页面进行缩放以适应屏幕
+    [mWebView setUserInteractionEnabled:YES];//是否支持交互
     //加载网络资源
     NSURL *url = [NSURL URLWithString:@"http://news.163.com/16/0525/09/BNTC1B7O00014PRF.html"];//创建url（统一资源定位符，互联网标准资源的地址）
     NSURLRequest *urlRequest = [[NSURLRequest alloc]initWithURL:url];//创建NSURLRequest
-    [webView loadRequest:urlRequest];//加载
+    [mWebView loadRequest:urlRequest];//加载
     /*
      //加载本地资源
     NSString* filePath = [[NSBundle mainBundle]pathForResource:@"1" ofType:@"jpg"];//本地资源路径
@@ -38,7 +38,7 @@
 
      */
     //设置代理
-    webView.delegate = self;
+    mWebView.delegate = self;
     
     //创建回退按钮
     UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(10, self.view.bounds.size.height - 80, 80, 50)];
@@ -60,23 +60,23 @@
     [reloadBtn addTarget:self action:@selector(reloaddata) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:reloadBtn];
     //显示webView
-    [self.view addSubview:webView];
+    [self.view addSubview:mWebView];
 
 }
 
 -(void)goback
 {
-    [webView goBack];
+    [mWebView goBack];
 }
 
 -(void)goforward
 {
-    [webView goForward];
+    [mWebView goForward];
 }
 
 -(void)reloaddata
 {
-    [webView reload];
+    [mWebView reload];
 }
 
 //当返回值为NO，表示不允许加载此请求
